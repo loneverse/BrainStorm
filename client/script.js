b21820217aaa86608937bc41764f3fe30a9dbe47
Ayub <ayub.njambuya@opibus.se>
2023-02-22 14:15:25 +0300

    updated Post request url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:7000", {
+  const response = await fetch("https://brainstorm-mpyi.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
